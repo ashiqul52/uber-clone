b550bcd18b6cce35251853b96ca3d09a34e1ece4
ashiqul52 <ashiqulislam3272@gmail.com>
2025-05-31 08:04:22 -0700

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "martins-docker-tfstate" # Replace with your actual S3 bucket name
+    bucket = "ashiq-docker-tfstate" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
